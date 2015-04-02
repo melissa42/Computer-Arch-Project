37f4b2ff740469dd35649020bb00a751b01b580f
Tyler Hannis <TylerHannis@gmail.com>
2015-04-02 11:00:24 -0500

    Class template attempted

--- components.h
+++ components.h
@@ -11,4 +11,21 @@
 
 #include <stdio.h>
 
+
+class Program_Counter{
+private:
+    static unsigned int instruction_address = 0;
+public:
+    int Program_Counter_Mux_Input(short x);
+    
+    /* 
+        The first variable passed will be the input, obtained for the
+        previous function, the second will be the first mux input and the
+        third will be the second mux input
+     */
+    
+    int set_Program_Counter_instruction_address(int x, int y, int z);
+    
+};
+
 #endif /* defined(____components__) */
--- main.c
+++ main.c
@@ -7,3 +7,8 @@
 // This file will server as the implementation of the data path, and the accompying files will be the development and digital construction of a 16-bit MIPS architecture simulator.
 
 #include <stdio.h>
+
+int main(){
+    
+    return 0;
+}
